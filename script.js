bb3f6f34a1dc4ddc51f0f2216b97bc75d1fa2943
Darkiton <160068056+Dar-ki-ton@users.noreply.github.com>
2024-03-21 13:12:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("btn1").addEventListener("click", function () {
+  alert(`science`);
+});
+
+document.getElementById("btn2").addEventListener("click", function () {
+  alert(`corners`);
+});
+
+document.getElementById("btn3").addEventListener("click", function () {
+  alert(`hand`);
+});
+
+document.getElementById("btn4").addEventListener("click", function () {
+  alert(`drawing board`);
+});
+
+document.getElementById("btn5").addEventListener("click", function () {
+  alert("leg");
+});
